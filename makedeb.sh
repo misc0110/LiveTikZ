96ba031e083b0f366a5d6199305b07a0ad6b49da
Michael Schwarz <michael.schwarz@student.tugraz.at>
2017-09-25 22:53:48 +0200

    debian package generation

---
+++ makedeb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if test `whoami` != "root"; then echo "You need to run this target using fakeroot: fakeroot -u make deb"; exit 1; fi
+mkdir -pv livetikz/usr/bin
+mkdir -pv livetikz/usr/share/doc/livetikz/
+cp build/livetikz livetikz/usr/bin
+strip livetikz/usr/bin/livetikz
+mkdir -p livetikz/DEBIAN
+sed "s/%VERSION%/0.1/" docs/debian-control > livetikz/DEBIAN/control
+echo "initial version" > livetikz/usr/share/doc/livetikz/changelog
+echo "" > livetikz/usr/share/doc/livetikz/copyright
+gzip -c -9 livetikz/usr/share/doc/livetikz/changelog > livetikz/usr/share/doc/livetikz/changelog.gz
+rm livetikz/usr/share/doc/livetikz/changelog
+chmod 0644 livetikz/usr/share/doc/livetikz/copyright
+chmod 0644 livetikz/usr/share/doc/livetikz/changelog.gz
+chmod -R 0755 livetikz/usr
+chown -R root:root livetikz/
+dpkg-deb --build livetikz
+rm -rf livetikz
+lintian livetikz.deb 
