3ef779e77dc9e62ee4db5ac441044dc7f508cfd9
Samuel Weiser <samuel.weiser@iaik.tugraz.at>
2018-12-06 18:21:40 +0100

    Watchers added, workdir changed to template dir, log cleared on recompile

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,15 +13,32 @@
 #include <QTemporaryFile>
 #include <QFile>
 #include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
 
 #include <QtCore/QDebug>
 #undef QT_NO_DEBUG
 #include <kdebug.h>
 
+QString getFilePath(QTemporaryDir* dir, const char* file) {
+  QString path(dir->path());
+  path.append("/");
+  path.append(file);
+  return path;
+}
 
 void MainWindow::textChanged(KTextEditor::Document *document) {
   (void)document;
   refreshTimer->start(1000);
+  watchme(texdir.path());
+}
+
+void MainWindow::documentSaved(KTextEditor::Document *document, bool saveas) {
+  (void)document;
+  (void)saveas;
+  std::cout << "Document saved" << std::endl;
+  usersaved = true;
 }
 
 void MainWindow::render() { render(display->getScale()); }
@@ -57,8 +74,10 @@ void MainWindow::render(double scale) {
 }
 
 void MainWindow::compile() {
+  usersaved = false;
   QSettings settings;
   QString program = settings.value("compiler", "pdflatex").toString();
+
   QStringList arguments;
   QTemporaryDir tmpdir;
   tmpdir.setAutoRemove(true);
@@ -66,30 +85,43 @@ void MainWindow::compile() {
   if (program == "pdflatex") {
     arguments << "-halt-on-error";
   }
-  if(dir->path() == "") {
+  if(!dir || dir->path() == "") {
     return;
   }
   if(texdir.absolutePath() == "") {
     std::cout << "need a temporary directory" << std::endl;
     texdir = QFileInfo(tmpdir.path());
   }
-  
-  arguments << dir->filePath("_livetikz_preview.tex");
+  QFileInfo templateDir = QFileInfo(templateFile.path());
+  if (templateDir.absolutePath() == "") {
+    workdir = texdir;
+  } else {
+    workdir = templateDir;
+  }
 
-  std::cout << "Arguments: " << dir->filePath("_livetikz_preview.tex").toStdString() << std::endl;
-  std::cout << "WD: " << texdir.absolutePath().toStdString() << std::endl;
-  
+  QString odir("--output-directory=");
+  odir.append(dir->path());
+  arguments << odir;
+
+  arguments << getFilePath(dir, "_livetikz_preview.tex");
+
+  std::cout << "Compiler arguments: " << arguments.join(QChar(' ')).toStdString() << std::endl;
+  std::cout << "Compiler workdir: " << workdir.absolutePath().toStdString() << std::endl;
+
   renderProcess = new QProcess(this);
-  renderProcess->setWorkingDirectory(texdir.absolutePath());
+  //~ renderProcess->setWorkingDirectory(texdir.absolutePath());
+  renderProcess->setWorkingDirectory(workdir.absolutePath());
+  clearLog();
   renderProcess->start(program, arguments);
 
   log->setText("Compiling...");
   renderOutput = "";
   connect(renderProcess, SIGNAL(finished(int)), this, SLOT(renderFinished(int)));
-  connect(renderProcess, SIGNAL(errorOccurred(QProcess::ProcessError)), this, SLOT(renderFailed(QProcess::ProcessError)));
+  //connect(renderProcess, SIGNAL(errorOccurred(QProcess::ProcessError)), this, SLOT(renderFailed(QProcess::ProcessError)));
   connect(renderProcess, SIGNAL(readyReadStandardError()), this, SLOT(updateLog()));
   connect(renderProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(updateLog()));
-  
+  connect(&templateWatcher, SIGNAL(fileChanged(const QString&)), this, SLOT(handleTemplateChanged(const QString&)));
+
   connect(killButton, SIGNAL(clicked()), renderProcess, SLOT(kill()));
   killButton->setVisible(true);
 }
@@ -101,7 +133,8 @@ void MainWindow::refresh() {
   }
 
   if (dir->isValid()) {
-    QFile file(dir->filePath("_livetikz_preview.tex"));
+    //~ QFile file(dir->filePath("_livetikz_preview.tex"));
+    QFile file(getFilePath(dir, "_livetikz_preview.tex"));
 
     if (file.open(QIODevice::ReadWrite | QIODevice::Text)) {
       QTextStream out(&file);
@@ -148,9 +181,14 @@ void MainWindow::refresh() {
       }
     }
   } else if(dir) {
+      std::cout << "Deleting invalid dir" << dir->path().toStdString() << std::endl;
       delete dir;
       dir = NULL;
   }
+}
+
+void MainWindow::clearLog() {
+  renderOutput = QString();
 }
 
 void MainWindow::appendLog(QString str) {
@@ -165,6 +203,7 @@ void MainWindow::appendLog(QString str) {
 void MainWindow::updateLog() {
   if (renderProcess) {
     appendLog(renderProcess->readAllStandardOutput());
+    appendLog(renderProcess->readAllStandardError());
   }
 }
 
@@ -188,24 +227,24 @@ void MainWindow::renderFinished(int code) {
   delete currentDoc;
   currentDoc = NULL;
 
-  QFile pdf_file(QDir::cleanPath(texdir.absolutePath() + QDir::separator() + "_livetikz_preview.pdf"));
-  if (pdf_file.exists()) {
-    QFile::rename(QDir::cleanPath(texdir.absolutePath() + QDir::separator() + "_livetikz_preview.pdf"), dir->filePath("_livetikz_preview.pdf"));
-    currentDoc = Poppler::Document::load(dir->filePath("_livetikz_preview.pdf"));
+  //~ QFile pdf_file(QDir::cleanPath(getFilePworkdir.absolutePath() + QDir::separator() + "_livetikz_preview.pdf"));
+  //~ if (pdf_file.exists()) {
+    //~ QFile::rename(QDir::cleanPath(texdir.absolutePath() + QDir::separator() + "_livetikz_preview.pdf"), dir->filePath("_livetikz_preview.pdf"));
+    //~ QFile::rename(QDir::cleanPath(workdir.absolutePath() + QDir::separator() + "_livetikz_preview.pdf"), getFilePath(dir, "_livetikz_preview.pdf"));
+    //~ currentDoc = Poppler::Document::load(dir->filePath("_livetikz_preview.pdf"));
+    QFile::remove(getFilePath(dir, "livetikz_preview.pdf"));
+    QFile::rename(getFilePath(dir, "_livetikz_preview.pdf"), getFilePath(dir, "livetikz_preview.pdf"));
+    
+    currentDoc = Poppler::Document::load(getFilePath(dir, "livetikz_preview.pdf"));
     if (currentDoc) {
       currentDoc->setRenderHint(Poppler::Document::TextAntialiasing);
       currentDoc->setRenderHint(Poppler::Document::Antialiasing);
       currentDoc->setRenderHint(Poppler::Document::TextHinting);
       currentDoc->setRenderHint(Poppler::Document::TextSlightHinting);
       currentDoc->setRenderHint(Poppler::Document::ThinLineSolid);
-
       render();
     }
-  }
-
-  dir->remove();
-  delete dir;
-  dir = NULL;
+  //~ }
 
   renderProcess = NULL;
 }
@@ -220,6 +259,7 @@ MainWindow::MainWindow() : currentDoc(NULL), renderProcess(NULL), currentPage(0)
 
   dir = NULL;
   templateFile = QUrl(settings.value("template", "").toString());
+  updateTemplate(templateFile.path());
   templateLabel->setText(templateFile.url(QUrl::PreferLocalFile));
 
   doc->setHighlightingMode("Latex");
@@ -237,7 +277,10 @@ MainWindow::MainWindow() : currentDoc(NULL), renderProcess(NULL), currentPage(0)
 
   connect((QObject *)doc, SIGNAL(textChanged(KTextEditor::Document *)), this,
           SLOT(textChanged(KTextEditor::Document *)));
-  connect(templateLabel, SIGNAL(textEdited(const QString&)), this, SLOT(updateTemplate(const QString&)));
+  connect(templateLabel, SIGNAL(textEdited(const QString&)), this, 
+          SLOT(updateTemplate(const QString&)));
+  connect((QObject *)doc, SIGNAL(documentSavedOrUploaded(KTextEditor::Document *, bool)), this,
+          SLOT(documentSaved(KTextEditor::Document *, bool)));
 }
 
 void MainWindow::showCompilerSelection() {
@@ -252,15 +295,51 @@ void MainWindow::showCompilerSelection() {
   }
 }
 
-MainWindow::~MainWindow() {}
+MainWindow::~MainWindow() {
+  /* remove temp dir */
+  if (dir) {
+    dir->remove();
+    delete dir;
+    dir = NULL;
+  }
+  if (texfileWatcher) {
+    delete texfileWatcher;
+    texfileWatcher = NULL;
+  }
+}
+
+void MainWindow::watchme(const QString& filename) {
+  if (texfileWatcher) {
+    /* We're already watching */
+    return;
+  }
+  /* Some editors delete and re-create files on save.
+   * This somehow breaks our watcher. On re-watching, the event is
+   * fired twice, then three times ...
+   * To avoid this, completely destroy and re-connect the watcher.
+   */
+  texfileWatcher = new QFileSystemWatcher();
+  connect(texfileWatcher, SIGNAL(fileChanged(const QString&)), this, SLOT(handleTexfileChanged(const QString&)));
+
+  std::cout << "Watching " << filename.toStdString() << std::endl;
+  int retry_count = 10;
+  while(!texfileWatcher->addPath(filename) && retry_count--) {
+    /* If the file has not been re-created yet, wait some time */
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout << "Retrying to watch..." << std::endl;
+  }
+}
 
 void MainWindow::load(const QUrl &url) { 
-    texdir = QFileInfo(url.toLocalFile());
-    katePart->openUrl(url); 
+  std::cout << "Loading " << url.toString().toStdString() << std::endl;
+  texdir = QFileInfo(url.toLocalFile());
+  watchme(url.toLocalFile());
+  katePart->openUrl(url); 
 }
 
 void MainWindow::setupActions() {
   KStandardAction::open(this, SLOT(load()), actionCollection());
+  //KStandardAction::save(this, SLOT(save()), actionCollection());
   KStandardAction::quit(qApp, SLOT(closeAllWindows()), actionCollection());
 }
 
@@ -350,9 +429,33 @@ void MainWindow::setupEditor() {
 
 void MainWindow::load() { load(QFileDialog::getOpenFileUrl()); }
 
+void MainWindow::handleTemplateChanged(const QString& filename) {
+  std::cout << filename.toStdString() << " changed." << std::endl;
+  updateTemplate(filename);
+}
+
+void MainWindow::handleTexfileChanged(const QString& filename) {
+  if (!usersaved) {
+    std::cout << "Texfile " << filename.toStdString() << " externally changed." << std::endl;
+    /* Reloading */
+    katePart->openUrl(QUrl(QUrl::fromUserInput(filename)));
+  }
+  /* Always re-watch file */
+  if (texfileWatcher) {
+    delete texfileWatcher;
+    texfileWatcher = NULL;
+  }
+  watchme(filename);
+}
+
 void MainWindow::updateTemplate(const QString& filename) {
   QSettings settings;
+  QUrl oldTemplateFile = QUrl(settings.value("template", "").toString());
+  templateWatcher.removePath(oldTemplateFile.path());
+
   templateFile = QUrl::fromUserInput(filename);
+  std::cout << "Loading template " << templateFile.path().toStdString() << std::endl;
+  templateWatcher.addPath(templateFile.path());
   settings.setValue("template", filename);
   refresh();
 }
