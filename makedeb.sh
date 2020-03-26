ccfa6e2094be091af926a189053b50434d0ebe72
Michael Schwarz <michael.schwarz91@gmail.com>
2020-03-26 22:09:58 +0100

    man file in deb

--- makedeb.sh
+++ makedeb.sh
@@ -2,6 +2,7 @@
 if test `whoami` != "root"; then echo "You need to run this target using fakeroot: fakeroot -u make deb"; exit 1; fi
 mkdir -pv livetikz/usr/bin
 mkdir -pv livetikz/usr/share/doc/livetikz/
+mkdir -pv livetikz/usr/share/man/man1/
 cp build/livetikz livetikz/usr/bin
 strip livetikz/usr/bin/livetikz
 mkdir -p livetikz/DEBIAN
@@ -9,10 +10,12 @@ sed "s/%VERSION%/$(cat VERSION)/" docs/debian-control > livetikz/DEBIAN/control
 echo "initial version" > livetikz/usr/share/doc/livetikz/changelog
 echo "Copyright 2020, Michael Schwarz" > livetikz/usr/share/doc/livetikz/copyright
 gzip -c -9 -n livetikz/usr/share/doc/livetikz/changelog > livetikz/usr/share/doc/livetikz/changelog.gz
+gzip -c -9 -n docs/livetikz.1 > livetikz/usr/share/man/man1/livetikz.1.gz
 rm livetikz/usr/share/doc/livetikz/changelog
 chmod -R 0755 livetikz/usr
 chmod 0644 livetikz/usr/share/doc/livetikz/copyright
 chmod 0644 livetikz/usr/share/doc/livetikz/changelog.gz
+chmod 0644 livetikz/usr/share/man/man1/livetikz.1.gz
 chown -R root:root livetikz/
 dpkg-deb --build livetikz
 rm -rf livetikz
